be8b3a01997841070c9fe96263211f4e9f9ff8d5
Paulo Victor <pv.35@hotmail.com>
2024-12-18 17:35:53 +0000

    init_player

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 10:14:46 by paude-so          #+#    #+#             */
-/*   Updated: 2024/12/18 17:29:49 by paude-so         ###   ########.fr       */
+/*   Updated: 2024/12/18 17:35:09 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -1106,31 +1106,49 @@ void	load_player_animations(t_player *player)
 		PLAYER_IDLE_ANIMATION_SPEED);
 }
 
-static void	init_player(void)
+static void init_player_position(t_player *player, t_map *map)
 {
-	t_player	*player;
-	t_map		*map;
+    init_entity_position(map, 'P', &player->x, &player->y);
+    player->state = IDLE_RIGHT;
+    player->last_direction = MOVE_RIGHT;
+}
 
-	player = &get_game()->player;
-	map = &get_game()->map;
-	init_entity_position(map, 'P', &player->x, &player->y);
-	load_player_animations(player);
-	player->state = IDLE_RIGHT;
-	player->last_direction = MOVE_RIGHT;
-	player->lives = 3;
-	player->state = IDLE_RIGHT;
-	player->last_direction = MOVE_RIGHT;
-	player->lives = 3;
-	player->invincibility_frames = 0;
-	player->is_visible = 1;
-	player->attack_cooldown = 0;
-	player->max_attack_cooldown = ATTACK_COOLDOWN;
-	player->is_attacking = false;
-	player->attack_frame = 0;
-	player->is_sprinting = false;
-	player->sprint_duration = 0;
-	player->sprint_cooldown = 0;
-	player->can_sprint = true;
+static void init_player_stats(t_player *player)
+{
+    player->lives = 3;
+    player->invincibility_frames = 0;
+    player->is_visible = 1;
+}
+
+static void init_player_combat(t_player *player)
+{
+    player->attack_cooldown = 0;
+    player->max_attack_cooldown = ATTACK_COOLDOWN;
+    player->is_attacking = false;
+    player->attack_frame = 0;
+}
+
+static void init_player_movement(t_player *player)
+{
+    player->is_sprinting = false;
+    player->sprint_duration = 0;
+    player->sprint_cooldown = 0;
+    player->can_sprint = true;
+}
+
+void init_player(void)
+{
+    t_player *player;
+    t_map *map;
+
+    player = &get_game()->player;
+    map = &get_game()->map;
+    
+    init_player_position(player, map);
+    init_player_stats(player);
+    init_player_combat(player);
+    init_player_movement(player);
+    load_player_animations(player);
 }
 
 static void	handle_sprint(t_player *player, int *movement_speed)

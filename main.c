efd9b787632d2bdcefc9f667391c3bb347e8573b
Paulo Victor <pv.35@hotmail.com>
2024-12-18 17:28:21 +0000

    init_collectible

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 10:14:46 by paude-so          #+#    #+#             */
-/*   Updated: 2024/12/18 17:24:13 by paude-so         ###   ########.fr       */
+/*   Updated: 2024/12/18 17:27:56 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -482,55 +482,84 @@ void	update_collectible(void)
 	}
 	update_animation(&collectible->base);
 }
-
-static void	init_collectible(void)
+static int count_collectible_positions(t_map *map)
 {
-	t_collectible	*collectible;
-	t_map			*map;
-	int				i;
-	int				j;
-	int				count;
+    int count;
+    int i;
+    int j;
 
-	collectible = &get_game()->collectible;
-	map = &get_game()->map;
-	count = 0;
-	i = -1;
-	while (++i < map->height)
-	{
-		j = -1;
-		while (++j < map->width)
-		{
-			if (map->map[i][j] == 'C')
-				count++;
-		}
-	}
-	collectible->count = count;
-	collectible->x_positions = malloc(sizeof(int) * count);
-	collectible->y_positions = malloc(sizeof(int) * count);
-	collectible->collected = malloc(sizeof(int) * count);
-	count = 0;
-	i = -1;
-	while (++i < map->height)
-	{
-		j = -1;
-		while (++j < map->width)
-		{
-			if (map->map[i][j] == 'C')
-			{
-				collectible->x_positions[count] = j * SPRITE_SIZE;
-				collectible->y_positions[count] = i * SPRITE_SIZE;
-				collectible->collected[count] = 0;
-				count++;
-			}
-		}
-	}
-	collectible->base.sprites[0] = make_sprite("assets/collect/collect00.xpm");
-	collectible->base.sprites[1] = make_sprite("assets/collect/collect01.xpm");
-	collectible->base.sprites[2] = make_sprite("assets/collect/collect02.xpm");
-	collectible->base.sprites[3] = make_sprite("assets/collect/collect03.xpm");
-	collectible->base.sprites[4] = make_sprite("assets/ban_col.xpm");
-	init_animation(&collectible->base, 4, COLLECTIBLE_ANIMATION_SPEED);
-	get_game()->collectible_count = 0;
+    count = 0;
+    i = -1;
+    while (++i < map->height)
+    {
+        j = -1;
+        while (++j < map->width)
+            if (map->map[i][j] == 'C')
+                count++;
+    }
+    return (count);
+}
+
+static bool allocate_collectible_arrays(t_collectible *collectible, int count)
+{
+    collectible->x_positions = malloc(sizeof(int) * count);
+    collectible->y_positions = malloc(sizeof(int) * count);
+    collectible->collected = malloc(sizeof(int) * count);
+    if (!collectible->x_positions || !collectible->y_positions 
+        || !collectible->collected)
+        return (false);
+    return (true);
+}
+
+static void store_collectible_positions(t_collectible *collectible, t_map *map)
+{
+    int i;
+    int j;
+    int count;
+
+    count = 0;
+    i = -1;
+    while (++i < map->height)
+    {
+        j = -1;
+        while (++j < map->width)
+        {
+            if (map->map[i][j] == 'C')
+            {
+                collectible->x_positions[count] = j * SPRITE_SIZE;
+                collectible->y_positions[count] = i * SPRITE_SIZE;
+                collectible->collected[count] = 0;
+                count++;
+            }
+        }
+    }
+}
+
+static void load_collectible_sprites(t_collectible *collectible)
+{
+    collectible->base.sprites[0] = make_sprite("assets/collect/collect00.xpm");
+    collectible->base.sprites[1] = make_sprite("assets/collect/collect01.xpm");
+    collectible->base.sprites[2] = make_sprite("assets/collect/collect02.xpm");
+    collectible->base.sprites[3] = make_sprite("assets/collect/collect03.xpm");
+    collectible->base.sprites[4] = make_sprite("assets/ban_col.xpm");
+    init_animation(&collectible->base, 4, COLLECTIBLE_ANIMATION_SPEED);
+}
+
+void init_collectible(void)
+{
+    t_collectible *collectible;
+    t_map *map;
+    int count;
+
+    collectible = &get_game()->collectible;
+    map = &get_game()->map;
+    count = count_collectible_positions(map);
+    collectible->count = count;
+    if (!allocate_collectible_arrays(collectible, count))
+        exit_error();
+    store_collectible_positions(collectible, map);
+    load_collectible_sprites(collectible);
+    get_game()->collectible_count = 0;
 }
 
 static void	init_wall(void)

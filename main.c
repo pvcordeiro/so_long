fcb47a52e4f06b398045c7c3ce45b69fd6d08c5b
Paulo Victor <pv.35@hotmail.com>
2024-12-18 17:41:25 +0000

    init_enemy

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 10:14:46 by paude-so          #+#    #+#             */
-/*   Updated: 2024/12/18 17:38:37 by paude-so         ###   ########.fr       */
+/*   Updated: 2024/12/18 17:40:53 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -674,33 +674,53 @@ void	handle_enemy_movement(t_enemy *enemy)
 	}
 }
 
-void	init_enemy(void)
+static bool allocate_enemy_array(t_enemy_list *enemy_list, int count)
 {
-	t_enemy_list	*enemy_list;
-	t_map			*map;
-	int				i;
-	int				j;
-	int				count;
+    enemy_list->enemies = malloc(sizeof(t_enemy) * count);
+    if (!enemy_list->enemies)
+        return (false);
+    enemy_list->count = count;
+    return (true);
+}
 
-	enemy_list = &get_game()->enemy_list;
-	map = &get_game()->map;
-	enemy_list->count = count_map_char(map->map, map->height, map->width, 'X');
-	enemy_list->enemies = malloc(sizeof(t_enemy) * enemy_list->count);
-	count = 0;
-	i = -1;
-	while (++i < map->height)
-	{
-		j = -1;
-		while (++j < map->width)
-		{
-			if (map->map[i][j] == 'X')
-			{
-				init_enemy_animations(&enemy_list->enemies[count]);
-				init_enemy_state(&enemy_list->enemies[count], j, i);
-				count++;
-			}
-		}
-	}
+static void init_single_enemy(t_enemy *enemy, int x, int y)
+{
+    init_enemy_animations(enemy);
+    init_enemy_state(enemy, x, y);
+}
+
+static void setup_enemies(t_enemy_list *enemy_list, t_map *map)
+{
+    int i;
+    int j;
+    int count;
+
+    count = 0;
+    i = -1;
+    while (++i < map->height)
+    {
+        j = -1;
+        while (++j < map->width)
+        {
+            if (map->map[i][j] == 'X')
+                init_single_enemy(&enemy_list->enemies[count++], j, i);
+        }
+    }
+}
+
+void init_enemy(void)
+{
+    t_enemy_list *enemy_list;
+    t_map *map;
+    int count;
+
+    enemy_list = &get_game()->enemy_list;
+    map = &get_game()->map;
+    
+    count = count_map_char(map->map, map->height, map->width, 'X');
+    if (!allocate_enemy_array(enemy_list, count))
+        exit_error();
+    setup_enemies(enemy_list, map);
 }
 
 void	update_enemy(void)

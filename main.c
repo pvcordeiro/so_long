898a3c3c6dc7281081a5d9cc5ac648c5dae82fa6
Paulo Victor <pv.35@hotmail.com>
2024-12-18 16:59:38 +0000

    parse_map

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 10:14:46 by paude-so          #+#    #+#             */
-/*   Updated: 2024/12/18 16:53:06 by paude-so         ###   ########.fr       */
+/*   Updated: 2024/12/18 16:59:23 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -1579,57 +1579,89 @@ static bool	validate_map(t_map *map)
 	return (true);
 }
 
+static t_map	*init_map_info(void)
+{
+    t_map	*map_info;
+
+    map_info = malloc(sizeof(t_map));
+    if (!map_info)
+        return (NULL);
+    map_info->height = 0;
+    map_info->width = 0;
+    return (map_info);
+}
+
+static bool	allocate_map_rows(t_map *map_info)
+{
+    map_info->map = malloc(sizeof(char *) * (map_info->height + 1));
+    if (!map_info->map)
+    {
+        free(map_info);
+        return (false);
+    }
+    return (true);
+}
+
+static void	count_dimensions(t_map *map_info, int fd)
+{
+    char	*line;
+
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        if (map_info->height == 0)
+            map_info->width = ft_strlen(line) - 1;
+        map_info->height++;
+        free(line);
+    }
+}
+
+static bool	read_map_content(t_map *map_info, int fd)
+{
+    char	*line;
+    int		i;
+
+    i = 0;
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        map_info->map[i] = line;
+        i++;
+    }
+    map_info->map[i] = NULL;
+    if (!validate_map(map_info))
+    {
+        i = -1;
+        while (++i < map_info->height)
+            free(map_info->map[i]);
+        free(map_info->map);
+        free(map_info);
+        return (false);
+    }
+    return (true);
+}
+
 t_map	*parse_map(char *filename)
 {
-	t_map	*map_info;
-	char	*line;
-	int		fd;
-	int		i;
+    t_map	*map_info;
+    int		fd;
 
-	map_info = malloc(sizeof(t_map));
-	if (!map_info)
-		return (NULL);
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-	{
-		free(map_info);
-		return (NULL);
-	}
-	map_info->height = 0;
-	map_info->width = 0;
-	while ((line = get_next_line(fd)) != NULL)
-	{
-		if (map_info->height == 0)
-			map_info->width = ft_strlen(line) - 1;
-		map_info->height++;
-		free(line);
-	}
-	close(fd);
-	map_info->map = malloc(sizeof(char *) * (map_info->height + 1));
-	if (!map_info->map)
-	{
-		free(map_info);
-		return (NULL);
-	}
-	fd = open(filename, O_RDONLY);
-	i = 0;
-	while ((line = get_next_line(fd)) != NULL)
-	{
-		map_info->map[i] = line;
-		i++;
-	}
-	map_info->map[i] = NULL;
-	close(fd);
-	if (!validate_map(map_info))
-	{
-		i = -1;
-		while (++i < map_info->height)
-			free(map_info->map[i]);
-		free(map_info->map);
-		free(map_info);
-		return (NULL);
-	}
-	return (map_info);
+    map_info = init_map_info();
+    if (!map_info)
+        return (NULL);
+    fd = open(filename, O_RDONLY);
+    if (fd < 0)
+    {
+        free(map_info);
+        return (NULL);
+    }
+    count_dimensions(map_info, fd);
+    close(fd);
+    if (!allocate_map_rows(map_info))
+        return (NULL);
+    fd = open(filename, O_RDONLY);
+    if (!read_map_content(map_info, fd))
+        return (NULL);
+    close(fd);
+    return (map_info);
 }
 
 int	main(int argc, char **argv)

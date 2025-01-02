173209611599d21b152d08890bfda843883edbd1
Paulo De Souza Cordeiro <paude-so@c2r5s7.42lisboa.com>
2025-01-02 15:04:04 +0000

    made update animation more explicit

--- srcs/utils/animation.c
+++ srcs/utils/animation.c
@@ -6,7 +6,7 @@
 /*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 02:46:46 by paude-so          #+#    #+#             */
-/*   Updated: 2024/12/19 04:05:05 by paude-so         ###   ########.fr       */
+/*   Updated: 2025/01/02 15:02:35 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,9 @@ void	update_sprite_animation(t_animated_sprite *anim)
 	if (anim->anim_counter >= anim->anim_speed)
 	{
 		anim->anim_counter = 0;
-		anim->current_frame = (anim->current_frame + 1) % anim->frame_count;
+		anim->current_frame++;
+		if (anim->current_frame >= anim->frame_count)
+			anim->current_frame = 0;
 	}
 }
 
